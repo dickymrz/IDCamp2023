b577e867ce34fe5dda83633568e765d844d4abc7
dickymrz <47812110+dickymrz@users.noreply.github.com>
2023-09-26 13:51:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ st.write("Contoh")
 
 
 # Tampilkan gambar
-image_url = "https://drive.google.com/file/d/1vtKZl_1qc2y1Ejjnb5KuXKf2ZlQQbSdj/view?usp=drive_link"
+image_url = "https://drive.google.com/drive/folders/1pJFEVDmliwZPfoxTqjdgl7s8y3LNxnDI?hl=id"
 st.image(image_url, caption='Test', use_column_width=True)
 
