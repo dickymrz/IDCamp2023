301558c0966f6531843e42a4280e35d1e172043d
dickymrz <47812110+dickymrz@users.noreply.github.com>
2023-09-26 09:52:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 st.title("Dashboard Streamlit Sederhana")
 st.write("Contoh")
 
-image_url = 'https://github.com/dickymrz/IDCamp2023/blob/main/images/Top%2010%20Kategori%20Produk%20dengan%20Jumlah%20Ulasan%20Positif%20Tertinggi.png'
+image_url = 'https://raw.githubusercontent.com/dickymrz/IDCamp2023/main/images/Top%2010%20Kategori%20Produk%20dengan%20Jumlah%20Ulasan%20Positif%20Tertinggi.png'
 
 # Tampilkan gambar
 st.image(image_url, caption='Visualisasi Data', use_column_width=True)
