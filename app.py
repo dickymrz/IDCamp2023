de67edc8a9c678908c0632be90e3c35b9c565f4c
dickymrz <47812110+dickymrz@users.noreply.github.com>
2023-09-26 14:12:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 import streamlit as st
 
+# Gambar 1
 image_path = "images/Top 10 Kota dengan Jumlah Pemesanan Tertinggi.png"
+st.image(image_path, caption="images/Top 10 Kota dengan Jumlah Pemesanan Tertinggi.png", use_column_width=True)
 
-# Tampilkan gambar
-st.image(image_path, caption="Test", use_column_width=True)
+# Gambar 2
+image_path = "images/Top 10 Kategori Produk dengan Jumlah Ulasan Positif Tertinggi.png"
+st.image(image_path, caption="Top 10 Kategori Produk dengan Jumlah Ulasan Positif Tertinggi.png", use_column_width=True)
+
+# Gambar 3
+image_path = "images/Produk dengan Jumlah Penjualan Tertinggi dan Terendah.png"
+st.image(image_path, caption="Produk dengan Jumlah Penjualan Tertinggi dan Terendah.png", use_column_width=True)
+
+# Gambar 4
+image_path = "images/Total Pendapatan Penjualan Perusahaan.png"
+st.image(image_path, caption="Total Pendapatan Penjualan Perusahaan.png", use_column_width=True)
+
+# Gambar 5
+image_path = "Produk dengan Harga Tertinggi dan Terendah.png"
+st.image(image_path, caption="Produk dengan Harga Tertinggi dan Terendah.png", use_column_width=True)
