a983c81b24b1aec2d587cd652d1dad7d6d0efcb6
dickymrz <47812110+dickymrz@users.noreply.github.com>
2023-09-26 13:58:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.image(
             "https://drive.google.com/file/d/19BwOzCsaf4P9lZ-Wuwtj9MXt6YjELk3n/view?usp=drive_link",
-            width=400, # Manually Adjust the width of the image as per requirement
+            width=1800, # Manually Adjust the width of the image as per requirement
         )
 
