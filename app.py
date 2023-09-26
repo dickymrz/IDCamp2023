5095be00bf6756ea66383e870832b2841b60203b
dickymrz <47812110+dickymrz@users.noreply.github.com>
2023-09-26 09:50:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
 import streamlit as st
 st.title("Dashboard Streamlit Sederhana")
 st.write("Contoh")
+
+image_url = https://github.com/dickymrz/IDCamp2023/blob/main/images/Top%2010%20Kategori%20Produk%20dengan%20Jumlah%20Ulasan%20Positif%20Tertinggi.png
