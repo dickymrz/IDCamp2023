b5f52ec1e22ec29301ac6a4c7a771f0685dfa0e0
dickymrz <47812110+dickymrz@users.noreply.github.com>
2023-09-26 13:54:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 image_url = "https://drive.google.com/drive/folders/1pJFEVDmliwZPfoxTqjdgl7s8y3LNxnDI?hl=id"
-st.image(image_url, caption='Test', use_column_width=True)
+st.image(image_url, caption='Test')
 
