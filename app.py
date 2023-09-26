c5d0a2759f0e8214f6e69eb174642428297b0d38
dickymrz <47812110+dickymrz@users.noreply.github.com>
2023-09-26 13:54:02 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,5 @@
 import streamlit as st
-st.title("Dashboard Streamlit Sederhana")
-st.write("Contoh")
 
-
-# Tampilkan gambar
 image_url = "https://drive.google.com/drive/folders/1pJFEVDmliwZPfoxTqjdgl7s8y3LNxnDI?hl=id"
 st.image(image_url, caption='Test', use_column_width=True)
 
