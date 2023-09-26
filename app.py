53eed19eb1adc639299400f3815b8f6a2d0cacc1
dickymrz <47812110+dickymrz@users.noreply.github.com>
2023-09-26 14:18:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -13,20 +13,20 @@ image_path = "images/Produk dengan Jumlah Penjualan Tertinggi dan Terendah.png"
 st.image(image_path, caption="Produk dengan Jumlah Penjualan Tertinggi dan Terendah", use_column_width=True)
 
 # Gambar 4
-image_path = "images/Total Pendapatan Penjualan Perusahaan.png"
-st.image(image_path, caption="Total Pendapatan Penjualan Perusahaan", use_column_width=True)
-
-# Gambar 5
 image_path = "images/Produk dengan Harga Tertinggi dan Terendah.png"
 st.image(image_path, caption="Produk dengan Harga Tertinggi dan Terendah", use_column_width=True)
+
+# Gambar 5
+image_path = "images/Total Pendapatan Penjualan Perusahaan.png"
+st.image(image_path, caption="Total Pendapatan Penjualan Perusahaan", use_column_width=True)
 
 # Membagi layar menjadi dua kolom
 left_column, right_column = st.columns(2)
 
 # Tampilkan gambar di kolom kiri
 with left_column:
-    st.image("images/Produk dengan Jumlah Penjualan Tertinggi dan Terendah.png", caption="Produk dengan Jumlah Penjualan Tertinggi dan Terendah", use_column_width=True)
+    st.image("images/Produk dengan Jumlah Penjualan Tertinggi dan Terendah.png", use_column_width=True)
 
 # Tampilkan gambar di kolom kanan
 with right_column:
-    st.image("images/Produk dengan Harga Tertinggi dan Terendah.png", caption="Produk dengan Harga Tertinggi dan Terendah", use_column_width=True)
+    st.image("images/Produk dengan Harga Tertinggi dan Terendah.png", use_column_width=True)
