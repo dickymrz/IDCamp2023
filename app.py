215f0802d2b65dde8f9a9e1ebca298b9f4a6f390
dickymrz <47812110+dickymrz@users.noreply.github.com>
2023-09-26 14:01:51 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
 import streamlit as st
-from PIL import Image
 
-image = Image.open('https://drive.google.com/file/d/19BwOzCsaf4P9lZ-Wuwtj9MXt6YjELk3n/view?usp=drive_link')
-
-st.image(image, caption='Test')
+![Image Description](upload://86TA7dB3o4z6meIQX3TajkaklNx.jpeg)
 
