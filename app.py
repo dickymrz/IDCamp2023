111bc455241a18b629cceb2aef0dcf67226f0e24
dickymrz <47812110+dickymrz@users.noreply.github.com>
2023-09-26 09:57:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,4 +5,5 @@ st.write("Contoh")
 image_url = 'https://drive.google.com/drive/folders/1_x3bBERoy3jspSon-nmF4bcxsGofk2Ir?hl=id'
 
 # Tampilkan gambar
-st.image(image_url, caption='Visualisasi Data', use_column_width=True)
+st.image(image_url, caption='Visualisasi Data', use_column_width=True, clear_cache=True)
+
